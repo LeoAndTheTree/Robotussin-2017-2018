35108d933364a075ca5155975dcfd4d195bfb842
LeoAndTheTree <leoyunsong@gmail.com>
2017-09-07 17:59:55 -0400

    Day 2
    Finished hardware mapping
    Started working on the op mode of the club fair robots
    the pdf somehow changed again

--- ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PushbotTeleopTank_Iterative.java
+++ ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PushbotTeleopTank_Iterative.java
@@ -59,7 +59,7 @@ import org.firstinspires.ftc.robotcontroller.external.samples.HardwarePushbot;
 public class PushbotTeleopTank_Iterative extends OpMode{
 
     /* Declare OpMode members. */
-    org.firstinspires.ftc.robotcontroller.external.samples.HardwarePushbot robot       = new HardwarePushbot(); // use the class created to define a Pushbot's hardware
+    HardwarePushbotChanged robot       = new HardwarePushbotChanged(); // use the class created to define a Pushbot's hardware
                                                          // could also use HardwarePushbotMatrix class.
     double          clawOffset  = 0.0 ;                  // Servo mid position
     final double    CLAW_SPEED  = 0.02 ;                 // sets rate to move servo
@@ -104,8 +104,10 @@ public class PushbotTeleopTank_Iterative extends OpMode{
         // Run wheels in tank mode (note: The joystick goes negative when pushed forwards, so negate it)
         left = -gamepad1.left_stick_y;
         right = -gamepad1.right_stick_y;
-        robot.leftMotor.setPower(left);
-        robot.rightMotor.setPower(right);
+        robot.leftFrontMotor.setPower(left);
+        robot.leftBackMotor.setPower(left);
+        robot.rightFrontMotor.setPower(right);
+        robot.rightBackMotor.setPower(right);
 
         // Use gamepad left & right Bumpers to open and close the claw
         if (gamepad1.right_bumper)
